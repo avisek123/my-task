fe3cf8156361b2dfcd04535cb356390458903735
avisek123 <sahooavisek41@gmail.com>
2024-09-15 10:09:50 +0530

    component re-use

--- src/screens/Private/Details.tsx
+++ src/screens/Private/Details.tsx
@@ -1,44 +1,32 @@
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import React from 'react';
 import {useGetSingleEmpQuery} from '../../services/EmpApi';
-import {useRoute} from '@react-navigation/native';
+import {useFocusEffect, useRoute} from '@react-navigation/native';
 import {PrivateRootRouteProps} from '../../types/allRoutes';
 import {colors, wrapper} from '../../styles';
-import {Loader} from '../../components';
+import {Lists, Loader} from '../../components';
 
 const Details = () => {
   const {params} = useRoute<PrivateRootRouteProps<'Details'>>();
-  const {data, isLoading} = useGetSingleEmpQuery(params?.id);
+  const {data, isLoading, refetch} = useGetSingleEmpQuery(params?.id);
+  useFocusEffect(
+    React.useCallback(() => {
+      refetch();
+    }, [refetch]),
+  );
   return (
     <SafeAreaView style={[wrapper, {backgroundColor: colors.white}]}>
       {isLoading ? (
         <Loader />
       ) : (
-        <View style={styles.container}>
-          <Text style={styles.title}>Employee Name: {data?.name}</Text>
-          <Text style={styles.body}>Employee Email: {data?.email}</Text>
-          <Text style={styles.body}>Employee Id: {data?.id}</Text>
-        </View>
+        <Lists
+          email={data?.email ?? ''}
+          name={data?.name ?? ''}
+          id={data?.id ?? ''}
+        />
       )}
     </SafeAreaView>
   );
 };
 
 export default Details;
-
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 10,
-    marginHorizontal: 10,
-  },
-  title: {
-    fontSize: 18,
-    color: colors.black,
-    fontWeight: 'bold',
-  },
-  body: {
-    fontSize: 18,
-    color: colors.greyOne,
-    fontWeight: '500',
-  },
-});
